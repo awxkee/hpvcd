8d5e2a4d00f77b800b13ece54f3e787a071f6890
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2026-06-06 19:30:32 +0100

    Initial decoder

---
+++ app/src/main.rs
@@ -0,0 +1,35 @@
+use hpvcd::ImageBuffer;
+use image::{DynamicImage, RgbImage};
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let bytes = fs::read("./assets/aak444.heic").unwrap();
+    let instant = Instant::now();
+    let decoded = hpvcd::decode_heic(&bytes).unwrap();
+    let decoded_yuv = hpvcd::decode_heic_yuv(&bytes).unwrap();
+    println!("Decoded: {:?}", instant.elapsed());
+    println!("Decoded: {:?}", decoded_yuv.bit_depth);
+    println!(
+        "bit depth {}, orient {:?}",
+        decoded.bit_depth as u8, decoded.orientation
+    );
+    let diff = decoded.bit_depth.bits() - 8;
+    let img = DynamicImage::ImageRgb8(
+        RgbImage::from_vec(
+            decoded.width,
+            decoded.height,
+            match decoded.pixels {
+                ImageBuffer::Luma8(luma) => luma.iter().flat_map(|&x| [x, x, x]).collect(),
+                ImageBuffer::Luma16(luma) => luma
+                    .iter()
+                    .flat_map(|&x| [(x >> diff) as u8, (x >> diff) as u8, (x >> diff) as u8])
+                    .collect(),
+                ImageBuffer::Rgb8(rgb) => rgb.to_vec(),
+                ImageBuffer::Rgb16(rgb16) => rgb16.iter().map(|&x| (x >> diff) as u8).collect(),
+            },
+        )
+        .unwrap(),
+    );
+    img.save("./out.jpg").unwrap();
+}
